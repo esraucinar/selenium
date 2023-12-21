68fc8bf046d6df19a18dc2a028c2d84032ef55cb
Esra <148894125+esraucinar@users.noreply.github.com>
2023-12-22 00:18:52 +0300

    Update homework1.py

--- homework1.py
+++ homework1.py
@@ -56,9 +56,39 @@ class Test_SauceDemo:
             passwordInput = driver.find_element(By.ID,"password")
             passwordInput.send_keys("secret_sauce")
             sleep(5)
-            errorMessage = driver.find_element(By.XPATH, '//*[@id="login_button_container"]/div/form/div[3]/h3')
-            testResult = errorMessage.text =="Epic sadface: Sorry, this user has been locked out."
+            loginButton = driver.find_element(By.ID,"login-button")
+            loginButton.click()
+            errorMessage = driver.find_element(By.XPATH, "//*[@id='login_button_container']/div/form/div[3]/h3")
+            testResult = errorMessage.text == "Epic sadface: Sorry, this user has been locked out."
             print(f"TEST SONUCU: {testResult}")
 testClass = Test_SauceDemo()
 testClass.sorry()
+
+#-Kullanıcı adı "standard_user" şifre "secret_sauce" gönderildiğinde 
+#kullanıcı "/inventory.html" sayfasına gönderilmelidir. 
+#Giriş yapıldıktan sonra kullanıcıya gösterilen ürün sayısı "6" adet olmalıdır.
+class Test_SauceDemo:
+    def successfull_login(self):
+        driver = webdriver.Chrome()
+        driver.get("https://www.saucedemo.com")
+        driver.maximize_window() 
+        sleep(3)
+        usernameInput = driver.find_element(By.ID, "user-name")
+        usernameInput.send_keys("standard_user")
+        sleep(2)
+        passwordInput = driver.find_element(By.ID, "password")
+        passwordInput.send_keys("secret_sauce")
+        sleep(2)
+        loginButton = driver.find_element(By.ID , "login-button")
+        loginButton.click()
+        sleep(3)
+        listOfElement = driver.find_elements(By.CLASS_NAME, "inventory_item")
+        urunSayisi = len(listOfElement) == 6
+        print(len(listOfElement))
+        print(f"BEKLENEN SONUC : {urunSayisi}")
+
+testClass = Test_SauceDemo()
+testClass.successfull_login()
+
+
 
